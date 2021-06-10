eab8612cbb9604c503a55a12f641b9efdcf54caf
coalpha <22552895+coalpha@users.noreply.github.com>
2021-06-09 18:15:00 -0700

    retroactive commit
    You know, might as well make this a repo of sorts. Honestly, I
    should've just made commits even if I didn't wanna upload it to GH.
    I'll keep that in mind for the future.

---
+++ FrmMain.vb
@@ -0,0 +1,30 @@
+﻿Public Class PointlessButton
+  Private ReadOnly Key As Microsoft.Win32.RegistryKey
+  Public Sub New()
+    InitializeComponent()
+    Dim RegSoftware = My.Computer.Registry.CurrentUser.OpenSubKey("Software", True)
+    Key = RegSoftware.CreateSubKey("coalpha").CreateSubKey("PointlessButton")
+    Btn.Text = GetCount()
+  End Sub
+
+  Private Function GetCount() As UInteger
+    Dim NumCount = Key.GetValue("count")
+    If NumCount Is Nothing Then
+      Key.SetValue("count", 0, Microsoft.Win32.RegistryValueKind.DWord)
+      Return 0
+    Else
+      Return NumCount
+    End If
+  End Function
+
+  Private Sub BtnClick() Handles Btn.Click
+    Dim NumCount = Key.GetValue("count")
+    If NumCount Is Nothing Then
+      NumCount = 1
+    Else
+      NumCount += 1
+    End If
+    Btn.Text = NumCount
+    Key.SetValue("count", NumCount, Microsoft.Win32.RegistryValueKind.DWord)
+  End Sub
+End Class
